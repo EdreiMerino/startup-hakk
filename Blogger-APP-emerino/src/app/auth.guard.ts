666a5ae0d9ce7d0717489ce4933baa6fd39cb305
Edrei Merino <edreimerino@gmail.com>
2023-08-30 16:04:07 -0700

    Start of auth.guard.ts file and code clean up

---
+++ Blogger-APP-emerino/src/app/auth.guard.ts
@@ -0,0 +1,14 @@
+// import { AuthService } from './service/auth-service/auth.service';
+// import { CanActivateFn, Router } from '@angular/router';
+// import { inject } from '@angular/core';
+
+// export const AuthGuard: CanActivateFn = () => {
+//   const authService = inject(AuthService);
+//   const router = inject(Router);
+
+//   if (authService.isUserLogin()) {
+//     return true;
+//   } else {
+//     return router.parseUrl('/login');
+//   }
+// };
