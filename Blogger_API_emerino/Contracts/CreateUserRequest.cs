d3bd4d56e10989e0c3a3ab8d5004af9a8f0a9464
Edrei Merino <edreimerino@gmail.com>
2023-09-27 15:37:18 -0700

    Adding Blogger API Task

---
+++ Blogger_API_emerino/Contracts/CreateUserRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace Contracts
+{
+    public class CreateUserRequest
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+    }
+}
